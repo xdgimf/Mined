1485f0cd689f028e65185ed39cd17031c749ef35
markpanizales <makspanizales@gmail.com>
2017-07-02 09:45:02 +0800

    Update BottomSheetMore.java
    Code cleanup and refactoring

--- Mined/app/src/main/java/com/pocketmarket/mined/fragments/BottomSheetMore.java
+++ Mined/app/src/main/java/com/pocketmarket/mined/fragments/BottomSheetMore.java
@@ -28,7 +28,6 @@ import java.util.ArrayList;
 public class BottomSheetMore extends BottomSheetDialogFragment implements View.OnClickListener {
     private final static String TAG = "BottomSheetMore";
     private BottomSheetBehavior mBehavior;
-//    private String mAccessToken;
 
     ArrayList<String> mCreditList = new ArrayList<String>();
     ArrayList<String> mDebitList = new ArrayList<String>();
@@ -51,17 +50,10 @@ public class BottomSheetMore extends BottomSheetDialogFragment implements View.O
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-//        // get the shared preferences user value
-//        mAccessToken = Utils.getAccessToken(getActivity());
-
-//        new BalanceAnalyticsFetchr().execute(getBalanceAnalytics());
     }
 
     @Override
     public void setupDialog(Dialog dialog, int style) {
-//        super.setupDialog(dialog, style);
-
         View contentView = View.inflate(getContext(), R.layout.fragment_bottom_sheet, null);
 
         contentView.findViewById(R.id.item).setOnClickListener(this);
@@ -112,22 +104,10 @@ public class BottomSheetMore extends BottomSheetDialogFragment implements View.O
         startActivity(i);
     }
 
-//    private void initExpenseAnalytics(){
-//        new BalanceAnalyticsFetchr().execute(getBalanceAnalytics());
-//    }
-
     private void showProducts(){
         Intent i = new Intent(getActivity(), MinedActivity.class);
         startActivity(i);
     }
-
-//    /**
-//     * Url for the smart assistant
-//     * @return
-//     */
-//    private String getBalanceAnalytics(){
-//        return AppApi.URL_NAME + AppApi.BALANCE_ANALYTICS + mAccessToken;
-//    }
 
     /**
      *  Asynccall for the assistant
